92e8e5fa08c781592fe768dd049d3f89b6aa3d7e
Adedayoke <adedayoke2006@gmail.com>
2025-09-21 19:07:33 +0100

    Repsh test

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -46,15 +46,15 @@ function createEmployee(salary: number | string): Director | Teacher {
   }
 }
 
-function isDirector(employee: Director | Teacher): employee is Director {
+export function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
-function executeWork(employee: Director | Teacher): void {
+function executeWork(employee: Director | Teacher) {
   if (isDirector(employee)) {
-    console.log(employee.workDirectorTasks());
+    return employee.workDirectorTasks()
   } else {
-    console.log(employee.workTeacherTasks());
+    return employee.workTeacherTasks()
   }
 }
 
